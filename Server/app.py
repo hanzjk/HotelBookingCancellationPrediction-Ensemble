4a9f9324b1fb12615f0ec126dc00caa578e3cb81
hanzjk <hansijk@yahoo.com>
2023-03-11 04:46:35 +0530

    Added Files

--- Server/app.py
+++ Server/app.py
@@ -88,8 +88,8 @@ def predict_cancellation():
     
     response = jsonify({
         'prediction': int(data[0]),
-        'target0': round((((data[1])[0])[0])*100,2), #confirmation prob
-        'target1': round((((data[1])[0])[1])*100,2) #cancellation prob
+        'confirmation_probality': round((((data[1])[0])[0])*100,2), #confirmation prob
+        'cancellation_probality': round((((data[1])[0])[1])*100,2) #cancellation prob
 
     })
     response.headers.add('Access-Control-Allow-Origin', '*',)
